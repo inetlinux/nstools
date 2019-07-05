6684dfee8d21d7831cba21446d0a098e7c6746a6
Jing Li <lijing@example.com>
2019-07-05 21:53:52 +0800

    fix build nsmap

--- nstools/common.py
+++ nstools/common.py
@@ -2,11 +2,15 @@ import re
 from nstools.cmd import shell_out
 
 def build_netns_map():
-    ret = {}
+    nsmap = {}
+    names = []
     out = shell_out('ip netns')
     for line in out.splitlines():
+        a = line.split(' ')
+        if len(a) > 0:
+            names.append(a[0])
         m = re.search(r'([^\s]+)\s.*?([0-9]+)', line)
         if not m or m.lastindex != 2:
             continue
-        ret[m.group(2)] = m.group(1)
-    return ret
+        nsmap[m.group(2)] = m.group(1)
+    return nsmap, names
